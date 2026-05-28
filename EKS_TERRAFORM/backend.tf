6b2971b91d8bc350eacfcc176995f27d21b3e677
Gopal-hub-dev <gsahu194723@gmail.com>
2026-05-28 08:36:55 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-gopal-3" # Replace with your actual S3 bucket name
+    bucket = "devsecops-2048-gopals-3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
